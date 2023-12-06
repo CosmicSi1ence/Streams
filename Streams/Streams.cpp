dc58548130232632bb51d58b375f8f81daf610bc
Artem Zenevskiy <ekko385@gmail.com>
2023-12-06 23:15:25 +0300

    new design

--- Streams/Streams.cpp
+++ Streams/Streams.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 using namespace chrono;
 
-void threadFunction(atomic<double> & result) {
+void func(atomic<double> & result) {
     double tmp = 5.;
     for (int i = 0; i < 50; ++i) {
         for (int j = 0; j < 999999; ++j) {
@@ -16,34 +16,29 @@ void threadFunction(atomic<double> & result) {
 }
 
 int main() {
-
-    system_clock::time_point point_new, point_old;
-    duration<double> duration_now;
-
-    atomic<double> a = 5.;
+    atomic<double> a = 0;
 
     //  ------------------ 1 thread ------------------------
 
-    point_old = system_clock::now();
-    threadFunction(a);
-    point_new = system_clock::now();
-    duration_now = point_new - point_old;
+    system_clock::time_point point_1 = system_clock::now();
+    func(a);
+    system_clock::time_point point_2 = system_clock::now();
+    duration<double> duration_1_thread = point_2 - point_1;
     cout << "[1 thread]\ta = " << a << "\n";
-    cout << "[1 thread]\tDuration for 1 thread = " << duration_now.count() << " sec.\n\n";
+    cout << "[1 thread]\tDuration for 1 thread = " << duration_1_thread.count() << " sec.\n\n";
 
     //  ------------------ 2 threads -----------------------
-    a = 5.;
 
-    point_old = system_clock::now();
-    thread thr1(threadFunction, ref(a));
-    thread thr2(threadFunction, ref(a));
+    system_clock::time_point point_3 = system_clock::now();
+    thread thr1(func, ref(a));
+    thread thr2(func, ref(a));
     thr1.join();
     thr2.join();
-    point_new = system_clock::now();
-    duration_now = point_new - point_old;
+    system_clock::time_point point_4 = system_clock::now();
+    duration<double> duration_2_threads = point_4 - point_3;
 
     cout << "[2 threads]\ta = " << a << "\n";
-    cout << "[2 threads]\tDuration for 2 threads = " << duration_now.count() << " sec.\n\n";
+    cout << "[2 threads]\tDuration for 2 threads = " << duration_2_threads.count() << " sec.\n\n";
 
     //  -----------------------------------------------------
 
