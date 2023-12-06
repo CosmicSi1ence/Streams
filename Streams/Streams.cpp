9507769f2f5f1bc3c714cd453a859d3cd42c4d3c
Artem Zenevskiy <ekko385@gmail.com>
2023-12-06 18:44:01 +0300

    fix float to double

--- Streams/Streams.cpp
+++ Streams/Streams.cpp
@@ -1,12 +1,11 @@
 ﻿#include <iostream>
 #include <thread>
 #include <chrono>
-#include <math.h>
 
 using namespace std;
 using namespace chrono;
 
-void threadFunction(const int num_thread, atomic<double> & result) {
+void threadFunction(atomic<double> & result) {
     double tmp = 5.;
     for (int i = 0; i < 50; ++i) {
         for (int j = 0; j < 999999; ++j) {
@@ -21,30 +20,30 @@ int main() {
     system_clock::time_point point_new, point_old;
     duration<double> duration_now;
 
-    atomic<double> A{ 5.f };
+    atomic<double> a = 5.;
 
     //  ------------------ 1 thread ------------------------
 
     point_old = system_clock::now();
-    threadFunction(1, A);
+    threadFunction(a);
     point_new = system_clock::now();
     duration_now = point_new - point_old;
-    cout << "[1 thread]\tA = " << A << "\n";
-    cout << "[1 thread]\tDuration for threads = " << duration_now.count() << " sec.\n\n";
+    cout << "[1 thread]\ta = " << a << "\n";
+    cout << "[1 thread]\tDuration for 1 thread = " << duration_now.count() << " sec.\n\n";
 
     //  ------------------ 2 threads -----------------------
-    A = 5.f;
+    a = 5.;
 
     point_old = system_clock::now();
-    thread thr1(threadFunction, 1, ref(A));
-    thread thr2(threadFunction, 2, ref(A));
+    thread thr1(threadFunction, ref(a));
+    thread thr2(threadFunction, ref(a));
     thr1.join();
     thr2.join();
     point_new = system_clock::now();
     duration_now = point_new - point_old;
 
-    cout << "[2 threads]\tA = " << A << "\n";
-    cout << "[2 threads]\tDuration for threads = " << duration_now.count() << " sec.\n\n";
+    cout << "[2 threads]\ta = " << a << "\n";
+    cout << "[2 threads]\tDuration for 2 threads = " << duration_now.count() << " sec.\n\n";
 
     //  -----------------------------------------------------
 
